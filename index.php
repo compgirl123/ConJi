e427046cad62551c7a6ac249be9e8b359647cae6
Pakman888 <patrick.ph.chan@gmail.com>
2017-10-22 23:31:32 -0400

    Last files added
    Just included some vector icons (Font Awesome) and jquery files.

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <html>
+    <head>
+    <link rel="stylesheet" href="css/font-awesome.css">
 
+        
+    </head>
     <body>
         <?php
             echo 'Hello';
         ?>
+        <i class="fa fa-address-book" aria-hidden="true"></i>
+
     </body>
 
 </html>

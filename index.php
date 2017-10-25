9ed97e43d3ce2d3b6949e546511297540185fea6
Claudia Feochari <claudia.f.feochari@hotmail.com>
2017-10-25 15:38:11 -0400

    testing

--- index.php
+++ index.php
@@ -1,15 +1,5 @@
-<html>
-    <head>
-    <link rel="stylesheet" href="css/font-awesome.css">
 
-        
-    </head>
-    <body>
         <?php
             echo 'Hello';
         ?>
-        <i class="fa fa-address-book" aria-hidden="true"></i>
-
-    </body>
-
-</html>
+  

d87074fd8f4976e05110423236d40d325c1ac7b0
Claudia Feochari <claudia.f.feochari@hotmail.com>
2017-11-12 11:52:08 -0500

    Integration with Patrick's Code

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="favicon.ico">
@@ -18,6 +19,9 @@
 	<link href="css/custom.css" rel="stylesheet" type="text/css" >
  
       <style>
+body{
+  background-color:#5998C5;
+}
 html{
   position: relative;
   min-height: 100%;
@@ -327,7 +331,7 @@ height:100%;
 
   <body>
 
-    <nav id="headerNav" class="navbar headerPurple navbar-fixed-top" style="background-color: #5998C5;">
+    <!--<nav id="headerNav" class="navbar headerPurple navbar-fixed-top" style="background-color: #5998C5;">
       <div class="container" >
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -340,7 +344,7 @@ height:100%;
       <a class="navbar-brand" href="#">
 
 		  </a>
-          <!--<p style="padding-top: 2em;">The Student Life Market-</P> -->
+          <p style="padding-top: 2em;">The Student Life Market-</P> 
         </div>
           
         <div id="navbar" class="navbar-collapse collapse">
@@ -350,7 +354,7 @@ height:100%;
             </div>
               <button type="submit" class="btn btn-success">Post Ad</button>
           </form>
-        </div><!--/.navbar-collapse -->
+        </div></.navbar-collapse 
       </div>
         
     
@@ -384,18 +388,135 @@ height:100%;
 </div>      
 
       </div>
+    </nav>-->
+     <!-- Static navbar -->
+    <nav class="navbar navbar-default navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a href= "#">
+          <img src="img/Conji_Logo_header.png" alt="funky pic spot" style="width:40%;height:100%;">
+          </a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Bulletin Board<span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                  <li class="dropdown-header">Buy/Sell</li>
+                <li><a href="#">Clothing</a></li>
+                <li><a href="#">Books</a></li>
+                <li><a href="#">Electronics</a></li>
+                <li><a href="#">Musical Instruments</a></li>
+                
+                <li role="separator" class="divider"></li>
+                <li class="dropdown-header">Services</li>
+                <li><a href="#">Tutors</a></li>
+                <li><a href="#">Event Planners</a></li>
+                  <li><a href="#">Photographers</a></li>
+                <li><a href="#">Personal Trainers</a></li>
+                
+                  
+                <li role="separator" class="divider"></li>
+                <li class="dropdown-header">Rent</li>
+                <li><a href="#">Electronics</a></li>
+                <li><a href="#">Car</a></li>
+                  <li><a href="#">Apartments</a></li>
+                <li><a href="#">Wedding-Dresses</a></li>
+                 
+                    <li role="separator" class="divider"></li>
+                <li class="dropdown-header">Employment</li>
+                <li><a href="#">Industrial</a></li>
+                <li><a href="#">Office</a></li>
+                  <li><a href="#">Non-Profit</a></li>
+                <li><a href="#">Part-Time</a></li>
+         
+                   
+              </ul>
+            </li>
+            <li><a href="#about">About</a></li>
+              <li><a href="#contact">Contact</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+          <li><a href="static/register.html">Register</a></li>
+            <li><a href="static/login.html">Login</a></li>
+          
+            <li><a href="../navbar-fixed-top/">Post Ad</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
     </nav>
+
+
+     <div class="container">
+
+      <!-- Main component for a primary marketing message or call to action -->
+      <div class="jumbotron">
+        <div class="panel">
+        <h3>Search Options</h3>
+          <nav class="navbar  ">
+      <div class="container">
+        <div class="row">
+
+  <div id="navbar" class=" navbar-nav" >
+      <div class="btn-group">
+  <button type="button" class="btn btn-danger">Location</button>
+  <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    <span class="caret"></span>
+    <span class="sr-only">Toggle Dropdown</span>
+  </button>
+  <ul class="dropdown-menu">
+      <li class="dropdown-header">Montreal</li>
+    <li><a href="#">Action</a></li>
+    <li><a href="#">Another action</a></li>
+    <li><a href="#">Something else here</a></li>
+    <li role="separator" class="divider"></li>
+     <li class="dropdown-header">Montreal</li>
+    <li><a href="#">Action</a></li>
+    <li><a href="#">Another action</a></li>
+    <li><a href="#">Something else here</a></li>
+      <li role="separator" class="divider"></li>
+     <li class="dropdown-header">Montreal</li>
+    <li><a href="#">Action</a></li>
+    <li><a href="#">Another action</a></li>
+    <li><a href="#">Something else here</a></li>
+  </ul>
+</div>
+        </div><!--/.nav-collapse -->
+
+
+         <div class="input-group">
+      <input type="text" class="form-control" placeholder="Search for...">
+      <span class="input-group-btn">
+        <button class="btn btn-default" type="button">Go!</button>
+      </span>
+    </div><!-- /input-group -->
+
+</div><!-- /.row -->
+      </div>
+    </nav>   
+        </div>
+          
+      </div> <!-- /jumbotron -->
+
+
+
 
       
 
-        <div id="fastHotShopSection" class="container" style="background-color:  #D8DBE2; width:100%; margin-top: 12em; padding-top: 10px; padding-bottom: 10px;">
+       <div class="container " style="background-color:  #D8DBE2; width:100%;">
           
-   
-        <h1 style="color: black;" class="border">POPULAR SHOP</h1>
-            <div id="top3HotShops" class="top3HotShops">
+        
+        <h1 style="color: black;">POPULAR SHOP</h1>
+            <div id="top3HotShops" >
                 <ul class="ulHorizontal">
                     <li><a href=""><img src="img/nike.png" alt="choice1" style="width:200px;"></a></li>
-                    <li><a href=""><img src="img/Nikon-Logo.jpg" alt="choice2" style="width:200px;"></a></li>
+                    <li><a href=""><img src="img/nikon-logo.jpg" alt="choice2" style="width:200px;"></a></li>
                     <li><a href=""><img src="img/deserres-logo.png" alt="choice3" style="width:200px;"></a></li>
                 </ul>
             </div>
@@ -539,14 +660,6 @@ height:100%;
               <div id="categoryElement" class="col-md-6" style="padding:5; margin:0; background-color: #373F51;">
                 <h1 class="categoryTitle"><a href="#">Employment</a></h1>
 
-                  <div class="row" style="height: 200px; overflow:hidden; text-align:center; " ><a href="#" id="sBusiness">
-                    <div  class="bgimg-13">
-                        
-                          <div class="caption">
-                            <p class="border">Small Business</p>
-                          </div>
-                    </div>  </a>
-                  </div>
                   
                    <div class="row" style="height: 200px; overflow:hidden; text-align:center; " ><a href="#" id="eIndustrial">
                     <div  class="bgimg-14">
@@ -574,10 +687,22 @@ height:100%;
                           </div>
                     </div>  </a>
                   </div>
+                  
+                  
+                  <div class="row" style="height: 200px; overflow:hidden; text-align:center; " ><a href="#" id="sBusiness">
+                    <div  class="bgimg-13">
+                        
+                          <div class="caption">
+                            <p class="border">Part-Time</p>
+                          </div>
+                    </div>  </a>
+                  </div>
               </div>
             
-          </div>
-      </div>
+          </div>    
+        
+
+    </div> <!-- /container -->
       
  
       <footer class="footer">

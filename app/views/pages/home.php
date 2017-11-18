d0d47829af68d629d1a3bac1c42f2d466a4a1d57
Claudia Feochari <claudia.f.feochari@hotmail.com>
2017-11-18 14:59:24 -0500

    make it better

--- app/views/pages/home.php
+++ app/views/pages/home.php
@@ -108,7 +108,7 @@
             if((isset($_SESSION['name']))){
               echo "<form action='' method='post'>
                     <!--<button type='submit' name ='logout' value='true'>Logout</button>-->
-                    <a href="pages/logout">Logout</a>
+                    <a href="logout.php">Logout</a>
                 </form>";
             }
             if(isset($_POST['logout'])) {

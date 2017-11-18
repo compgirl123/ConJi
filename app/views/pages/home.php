b752882ea0422a93bdea7fd53f5b192ad6a3e4b1
Claudia Feochari <claudia.f.feochari@hotmail.com>
2017-11-18 13:56:48 -0500

    make it better

--- app/views/pages/home.php
+++ app/views/pages/home.php
@@ -12,8 +12,9 @@
     <title>Static Top Navbar Example for Bootstrap</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.css" rel="stylesheet">
- <link href="css/custom.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.css" media="all">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/custom.css" media="all">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/index.css" media="all">
       <style>
        
 .bgimg-1 {

fbb7aa28ae340d4ea4533322ff189f2af65ba1f0
Claudia Feochari <claudia.f.feochari@hotmail.com>
2017-11-17 21:09:08 -0500

    DD

--- app/models/Social.php
+++ app/models/Social.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Login extends CI_Model {
+class Social extends CI_Model {
 
     function __construct() {
         $this->tableName = 'users';

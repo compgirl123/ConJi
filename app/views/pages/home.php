da4fc281bdb7a70a51f65f4f20c6d026e8cea56a
Claudia Feochari <claudia.f.feochari@hotmail.com>
2017-11-18 14:30:15 -0500

    make it better

--- app/views/pages/home.php
+++ app/views/pages/home.php
@@ -350,11 +350,15 @@
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
+
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+       <!--customJavaScript-->    
     <script>window.jQuery || document.write('<script src="js/jquery.min.js"><\/script>')</script>
-    <script src="js/bootstrap.min.js"></script>
+
+    <script src="<?php echo base_url(); ?>assets/js/bootstrap.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="js/ie10-viewport-bug-workaround.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/ie10-viewport-bug-workaround.js"></script>
+
     </body>
     </html>

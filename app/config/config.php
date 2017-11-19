3642c409342d7547d7908e5741c51fb89bcc5671
Claudia Feochari <claudia.f.feochari@hotmail.com>
2017-11-18 20:38:38 -0500

    make it better

--- app/config/config.php
+++ app/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://conjiga.herokuapp.com/';
+$config['base_url'] = 'https://conji.ga/';
 
 /*
 |--------------------------------------------------------------------------

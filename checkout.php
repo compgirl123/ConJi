caebbd5c89106cacf31a14af9f2879fce6a32a1b
Pakman888 <patrick.ph.chan@gmail.com>
2017-11-24 08:18:01 -0500

    barebone checkout page
    made it as generic looking as possible. It should be versatile enough for the Membership Plan or Ad Exposure plans.

--- checkout.php
+++ checkout.php
@@ -10,144 +10,113 @@
     <meta name="author" content="">
     <link rel="icon" href="favicon.ico">
 
-    <title>Static Top Navbar Example for Bootstrap</title>
+    <title>Checkout</title>
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.css" rel="stylesheet">
- <link href="../css/custom.css" rel="stylesheet">
-      <style>
 
-       
-   .sellOptionsContainer{
-              border: 2px solid black;
-              
+      <style>
+          .radioSpacing{
+              margin:10px;
+              float:left;
+              left:0; right:0;
+              text-align:center;
+          }
+          .inputBoxSpacing{
+             float:right;
+              margin-left:5px;
+          }
+          .colSpacing{
+              margin-top:8px;
           }
           
-/*     */
-.footer{
-    margin-top:10px;
-    padding:10px;
-    
-    position:absolute;
-    width:100%;
-    bottom:0;
-    text-align:center;
-    background-color: #1B1B1E;
-}
+          
       </style>
-
-      <script>
-      /*****
-        display image when it's picked.
-      ****/    
-           function PreviewImage() {
-        var oFReader = new FileReader();
-        oFReader.readAsDataURL(document.getElementById("selectImg").files[0]);
-
-        oFReader.onload = function (oFREvent) {
-            document.getElementById("adImg").src = oFREvent.target.result;
-        };
-    };
-          /*******************/
-          
-         
-          
-      </script>
   </head>
 
   <body>
 
 
     <!-- Static navbar -->
-    <nav class="navbar navbar-default navbar-static-top">
-      <div class="container">
-
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-
-          <a class="navbar-brand" href="../">Project name</a>
-
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-
-          <ul class="nav navbar-nav">
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Bulletin Board<span class="caret"></span></a>
-              <ul class="dropdown-menu">
-                  <li class="dropdown-header">Buy/Sell</li>
-                <li><a href="#">Clothing</a></li>
-                <li><a href="#">Books</a></li>
-                <li><a href="#">Electronics</a></li>
-                <li><a href="#">Musical Instruments</a></li>
-                
-                <li role="separator" class="divider"></li>
-                <li class="dropdown-header">Services</li>
-                <li><a href="#">Tutors</a></li>
-                <li><a href="#">Event Planners</a></li>
-                  <li><a href="#">Photographers</a></li>
-                <li><a href="#">Personal Trainers</a></li>
-                
-                  
-                <li role="separator" class="divider"></li>
-                <li class="dropdown-header">Rent</li>
-                <li><a href="#">Electronics</a></li>
-                <li><a href="#">Car</a></li>
-                  <li><a href="#">Apartments</a></li>
-                <li><a href="#">Wedding-Dresses</a></li>
-                 
-                    <li role="separator" class="divider"></li>
-                <li class="dropdown-header">Employment</li>
-                <li><a href="#">Industrial</a></li>
-                <li><a href="#">Office</a></li>
-                  <li><a href="#">Non-Profit</a></li>
-                <li><a href="#">Part-Time</a></li>
-         
-                   
-              </ul>
-            </li>
-            <li><a href="../about">About</a></li>
-              <li><a href="../contact">Contact</a></li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="../navbar/">Login</a></li>
-          
-            <li><a href="">Post Ad</a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </nav>
-
-
-
-    <div class="container">
+  <div class="container">
       <!-- Main component for header -->
 	  <form runat="server" id="adForm" method="post" action="">
 	  
       <div class="jumbotron">
         <div class="panel" style="padding:10px;">
-        <h3>Check Out Information</h3>
+        <h2>Check Out Information</h2>
           
-		  <div class="container">
-			<label>First Name: </label><input type="text" name="fname" class="fname" id="fname" required>
-            <label>Last Name: </label><input type="text" name="lname" class="lname" id="lname" required>
-              <label>E-mail: </label><input type="text" name="email" class="email" id="email" required>
-              <hr>
-              <h3>Payment Options</h3>
-              <label>Visa </label> <input type="radio" name="credName" id="credName" class="credName" value="visa">
-              <label>Master Card </label> <input type="radio" name="credName" id="credName" class="credName" value="masterC">
-              <label>American Express </label> <input type="radio" name="credName" id="credName" class="credName" value="ae">
-              <label>Paypal </label> <input type="hidden" name="cmd" value="_s-xclick">
-  <input type="hidden" name="hosted_button_id" value="6RNT8A4HBBJRE">
-  <input type="image"
-    src="https://www.paypalobjects.com/webstatic/en_US/i/btn/png/btn_buynow_107x26.png" alt="Buy Now">
-  <img alt="" src="https://paypalobjects.com/en_US/i/scr/pixel.gif"
-    width="1" height="1">
-		  </div><!-- /container -->  
+            <form get="post" action="">
+              <div class="container">
+                  <h3>Contact Information</h3>
+                  <div class="row">
+                      
+                      <div class="col-md-3 "><label>First Name: </label><input type="text" name="fname" class="fname" id="fname" required></div>
+                      <div class="col-md-3 "><label>Last Name: </label><input type="text" name="lname" class="lname" id="lname" required></div>
+                      <div class="col-md-3 "> <label>E-mail: </label><input type="text" name="email" class="email" id="email" required></div>
+                      <div class="col-md-3 "> <label>Phone: </label><input type="text" name="phone" class="phone" id="phone" required></div>
+                  </div>
+                  <hr>
+                    <h3>Billed Address</h3>
+                     <div class="row">
+                      
+                      <div class="col-md-4 colSpacing"><label>Building Number</label><input type="text" name="bNumber" class="inputBoxSpacing" id="bNumber" required></div>
+                      <div class="col-md-4 colSpacing"><label>Street Name</label><input type="text" name="street" class="inputBoxSpacing" id="street" required></div>
+                      <div class="col-md-4 colSpacing"> <label>Apt</label><input type="text" name="apt" class="inputBoxSpacing" id="apt" required></div>
+                    </div>
+                  <div class="row">
+                       <div class="col-md-4 colSpacing "> <label>Postal Code </label><input type="text" name="pCode" class="inputBoxSpacing" id="pCode" required></div>
+                      <div class="col-md-4 colSpacing"><label>Province </label><input type="text" name="province" class="inputBoxSpacing" id="province" required></div>
+                      <div class="col-md-4 colSpacing"><label>Country</label><input type="text" name="country" class="inputBoxSpacing" id="country" required></div>
+                    </div>
+                  
+                  <hr>
+                    <h3>Payment Options</h3>
+                  
+                    <div class="row" style="width:100%; margin:0; ">
+                        <div class="radioSpacing"> <label>Visa </label> <input type="radio" name="credName" id="credName" class="inputSpacing" value="visa" required> </div>
+                        <div class="radioSpacing"><label>Master Card </label> <input type="radio" name="credName" id="credName" class="inputSpacing"   value="masterC" required>
+                        </div>
+                        <div class="radioSpacing"><label>American Express </label> <input type="radio" name="credName" id="credName" class="inputSpacing" value="ae" required>
+                        </div>
+                        <div class="radioSpacing"> <label>Card Number </label><input type="text" class="inputBoxSpacing" name="credNumberBox" id="credNumberBox" required></div>
+                    </div>
+                       
+                  <hr>
+
+                  <div class="container">
+                    <h3>Product Summary</h3>
+                    <label>Description</label>
+                    <div class="row">
+                        <div class="col-md-11">
+                          <p name="objDesc" id="objDesc">The awesome thing I am paying for.</p>
+                        </div>
+                        <div class="col-md-1">
+                          <label name="priceValue" id="priceValue" >$0.00</label>
+                        </div>
+                    </div>
+                  </div>
+                  <hr>
+                
+                    
+                    <div class="container">
+                    <div class="row">
+                        <div class="col-md-11"><label>Total Price</label></div>
+                          <div class="col-md-1"><label name="totalPriceValue" id="totalPriceValue" >$0.00</label></div>
+                         
+                       
+                    </div>
+                        </div>
+                  
+                  <div class="row">
+                    <div style="width:100%; ">
+                        <button type="submit" name="btnSubmit" id="btnSubmit" style="float:right; margin:10px;" class="btn btn-success">Pay Now</button> 
+                      </div>
+                  </div>
+                
+              </div><!-- /container -->
+            </form>
+            
 		</div><!-- /panel -->
 	</div><!-- /jumbotron -->
 

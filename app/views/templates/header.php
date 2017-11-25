7531e611c103cc77f3011ede2b3288c8e958cc9c
Claudia Feochari <claudia.f.feochari@hotmail.com>
2017-11-24 20:58:39 -0500

    Added custom name to profile page, fixed name on navbar

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -14,7 +14,7 @@
             <span class="icon-bar"></span>
           </button>
 
-          <a class="navbar-brand" href="#">Project name</a>
+          <a class="navbar-brand" href="#">ConJi</a>
 
         </div>
         <div id="navbar" class="navbar-collapse collapse">

90ebff73d43a7c4189d53b2f3e2e1da6cf1e7cc5
Marzie Shafiee <marziesh@Marzies-MacBook-Pro.local>
2017-11-25 15:38:31 -0500

    Add PHP to PostAd page.

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -85,6 +85,69 @@ class Pages extends CI_Controller{
 
 	}
 
+	function createTable()
+	{
+		// Connection to LocalHost
+		$servername = "us-cdbr-iron-east-05.cleardb.net";
+		$username = "bef91df46d38c7";
+		$password = "736a5a2e";
+		$dbname = "heroku_b8e3c8b5ba457b6";
+
+		// Create connection
+		$conn = new mysqli($servername, $username, $password,$dbname);
+
+		// Check connection
+		if ($conn->connect_error) {
+		    die("Connection failed: " . $conn->connect_error);
+		} 
+		else{
+
+		}
+
+		$create_table = "CREATE TABLE `heroku_b8e3c8b5ba457b6`.`postad_Ad` 
+						(
+						ad_id INT(6) AUTO_INCREMENT,
+						user_id INT(6),
+						type CHAR(4) CHECK (TYPE IN ('BUY', 'Sell')),
+						quantity INT(4),
+						contact_number INT(11),
+						contact_email VARCHAR(75),
+						sold_by VARCHAR(100),
+						title VARCHAR(50),
+						description VARCHAR(2000),
+						price DOUBLE (7, 2),
+						date_made DATE NOT NULL,
+						expired_ad BOOLEAN NOT NULL DEFAULT 0,
+						is_product BOOLEAN NOT NULL DEFAULT 0,
+						category VARCHAR(20) CHECK (category IN ('RentCar', 'RentElectronics', 'RentWedding', 'RentAppartment', 'Buy_SellClothing', 'Buy_SellBook', 'Buy_SellElectronics', 'Buy_SellInstrument', 'JobSmall','JobCorporation', 'JobNon_Profit', 'JobIndustrial', 'Service Tutor', 'ServiceEvent', 'ServicePhoto', 'ServiceTrainer')),
+						PRIMARY KEY (ad_id, user_id)
+						)";
+		$result = $conn->query($create_table);
+	}
+	function addAd()
+	{
+		$servername = "us-cdbr-iron-east-05.cleardb.net";
+		$username = "bef91df46d38c7";
+		$password = "736a5a2e";
+		$dbname = "heroku_b8e3c8b5ba457b6";
+
+		// Create connection
+		$conn = new mysqli($servername, $username, $password,$dbname);
+
+		// Check connection
+		if ($conn->connect_error) {
+		    die("Connection failed: " . $conn->connect_error);
+		} 
+
+		$this->createTable();
+		$Ad = "INSERT INTO `heroku_b8e3c8b5ba457b6`.`registered_users`(title,sold_by,type,quantity,contact_number,contact_email,description,price,date_made,expired_ad,category,is_product) 
+				 VALUES ("."'".$_POST['form-title']."','".$_POST['form-sold_by']."','".$_POST['form-type']."','".
+				 $_POST['form-email']."','".$_POST['form-quantity']."','".$_POST['form-contact_number']."','".$_POST['form-contact_email']."','".$_POST['form-sold_by']."','".$_POST['form-sold_by']."','".$_POST['form-sold_by']."','".$_POST['form-description']."','".$_POST['form-price']."','".$_POST['form-date_made']."','".$_POST['form-expired_ad']."','".$_POST['form-category']."','".$_POST['form-is_product']."')";
+
+		$result = $conn->query($Ad);
+	}
+
+
 
 
 	

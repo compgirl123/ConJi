e361379fb45b893ea1c41dd2a08db478edd8ac58
Claudia Feochari <claudia.f.feochari@hotmail.com>
2017-11-25 22:28:25 -0500

    Cleanup

--- app/models/Registration.php
+++ app/models/Registration.php
@@ -3,25 +3,8 @@
 Class Registration extends CI_Model
 {
 	
-	
 	function createTable()
 	{
-		// Connection to LocalHost
-		/*$servername = "us-cdbr-iron-east-05.cleardb.net";
-		$username = "bef91df46d38c7";
-		$password = "736a5a2e";
-		$dbname = "heroku_b8e3c8b5ba457b6";
-
-		// Create connection
-		$conn = new mysqli($servername, $username, $password,$dbname);
-
-		// Check connection
-		if ($conn->connect_error) {
-		    die("Connection failed: " . $conn->connect_error);
-		} 
-		else{
-
-		}*/
 		if ($this->db->table_exists('user'))
 		{
 		   $result = '';
@@ -41,7 +24,6 @@ Class Registration extends CI_Model
 						  `first_name` char(50) DEFAULT NULL,
 						  PRIMARY KEY (`user_id`)
 						)";
-			//var_dump($this->db->table_exists('user'));
 			$result = $this->db->query($create_table);
 		}
 		
@@ -51,20 +33,6 @@ Class Registration extends CI_Model
 
 	function addUser()
 	{
-		/*$servername = "us-cdbr-iron-east-05.cleardb.net";
-		$username = "bef91df46d38c7";
-		$password = "736a5a2e";
-		$dbname = "heroku_b8e3c8b5ba457b6";
-
-		// Create connection
-		$conn = new mysqli($servername, $username, $password,$dbname);
-
-		// Check connection
-		if ($conn->connect_error) {
-		    die("Connection failed: " . $conn->connect_error);
-		}*/ 
-
-		//$this->createTable();
 		$this->load->database();
 
 			 $user = "INSERT INTO `User`(first_name, last_name, password,email,city,province,country) 
@@ -72,11 +40,7 @@ Class Registration extends CI_Model
 				 $_POST['form-email']."','".$_POST['form-city']."','".$_POST['form-province']."','".$_POST['form-country']."')";
 
 			 $result = $this->db->query($user);
-		/*$user = "INSERT INTO `heroku_b8e3c8b5ba457b6`.`registered_users`(firstName, lastName, password,email,city,province,country) 
-				 VALUES ("."'".$_POST['form-fname']."','".$_POST['form-lname']."','".$_POST['form-password']."','".
-				 $_POST['form-email']."','".$_POST['form-city']."','".$_POST['form-province']."','".$_POST['form-country']."')";*/
 
-		//$result = $conn->query($user);
 	}
 }
 
